f85c5f2774b1c31c20c9eeb7657b96020d552c1e
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-27 22:31:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function iterativeLog(arr) {
   arr.forEach((x, idx) => console.log(`${idx}: ${x}`))
 }
+
+function iterate(callback) {
+  let arr = ['1','2','3']
+  arr.forEach(callback)
+  return arr
+}
